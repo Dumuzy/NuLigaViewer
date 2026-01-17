cab0dbebc2a45fe6d45e65cbd8ed452d896da7ca
Nico König <nichue@liquitcore.com>
2026-01-17 16:47:34 +0100

    Fix DWZ average for game days

--- Data/GameDay.cs
+++ Data/GameDay.cs
@@ -5,9 +5,9 @@ namespace NuLigaViewer.Data
         public DateTime Datum { get; set; }
         public int Runde { get; set; }
         public string? HeimMannschaft { get; set; }
-        public double HeimMannschaftDWZ => (Report != null && Report.Pairings.Count > 0) ? Math.Round(Report.Pairings.Average(x => x.HeimSpielerDWZ)) : 0;
+        public double HeimMannschaftDWZ => (Report != null) ? Report.AverageHomeDWZ : 0;
         public string? GastMannschaft { get; set; }
-        public double GastMannschaftDWZ => (Report != null && Report.Pairings.Count > 0) ? Math.Round(Report.Pairings.Average(x => x.GastSpielerDWZ)) : 0;
+        public double GastMannschaftDWZ => (Report != null) ? Report.AverageGuestDWZ : 0;
         public string? BrettPunkte { get; set; }
         public string? ReportUrl { get; set; }
         public GameReport? Report { get; set; }
